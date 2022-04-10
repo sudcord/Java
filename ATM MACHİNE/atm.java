56910d41667af2b32c3a44532de5c47872d6b903
Sudkostik <73934737+sudkostik@users.noreply.github.com>
2022-04-10 18:47:04 +0300

    Add files via upload

---
+++ ATM MACHİNE/atm.java
@@ -0,0 +1,44 @@
+// HEY LMAO 
+// HEY LMAO 
+// HEY LMAO 
+
+import java.util.Scanner;
+public class atm {
+public static void main(String[] args) {
+Scanner amongus = new Scanner(System.in);
+int sudcoin = 3169;
+System.out.println("1- Balance 2- Withdraw 3- Deposit");
+while (true) {
+System.out.println("Please make a choice -Sudbank"); // dababy der ki, less goooo
+int impostorsus = amongus.nextInt();
+switch (impostorsus) {
+case 1:
+System.out.println("You have " + sudcoin + " Sudcoin");
+break;
+case 2:
+System.out.println("Enter the amount of money you want to withdraw from the bank, silly, I'm not Allah (maybe).");
+int dangalakbotcu = amongus.nextInt();
+sudcoin -= dangalakbotcu;
+System.out.println("New balance " + sudcoin + "Sudcoin");
+break;
+case 3:
+System.out.println("Properly place the money you will deposit in your account. (Enter amount) -Sudbank");
+int dangalakbotcu1 = amongus.nextInt();
+sudcoin += dangalakbotcu1;
+System.out.println("New balance " + sudcoin + "Sudcoin");
+break;
+default:
+System.out.println("err");
+break;
+}
+System.out.println("Do you want to take another action? Because I will go to the pavilion with the interest I get. (y/n)");
+String choicetwo = amongus.next();// HEY LMAO 
+if(choicetwo.equals("y"))
+continue;
+if(choicetwo.equals("n")) // HEY LMAO 
+System.out.println("The card is being returned lol"); // HEY LMAO 
+break; }
+}
+}
+
+// HEY LMAO 
---
+++ ATM MACHİNE/readme.md
@@ -0,0 +1,9 @@
+## Hey hey how are you! I designed a simple economy game, how about you have a look? <br>
+
+**Step 1: download this sussy baka** <br>
+
+**Step 2: javac atm.java** <br>
+
+**Step 3: java atm.java** <br>
+
+*Note: JDK, Java Development Kit, must be installed on your device. Otherwise, the compiler wont work.*
